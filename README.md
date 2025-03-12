bfe40dee87d22d853ec77407566154b280cd6eaa
Evan Rosson <git@erosson.org>
2025-03-12 09:30:36 -0400

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Choirtube
+
+Generate consistently-formatted files for my choir practice. Used for Youtubeifying music of various formats.
+
+I like practicing music with the piano/the rest of the choir in the background. Youtube's a surprisingly good music player for this! You can easily link to chapter markers, instead of guessing what minute/second each section starts. You can use the create-clip interface to loop one section, to drill it repeatedly (without actually creating a clip).
+
+This repository deliberately does not name the choir I use it for. Hopefully this makes the tool more reusable, and also this repository is public and I like my privacy.
+
+# Usage 
+
+A Linux system with `ffmpeg` and `timidity` are required. (I should really provide these in a docker container...)
+
+One-time setup:
+
+* Move the image to use for videos to `assets/youtube.png`.
+
+To add new music:
+
+* Drop new music into `./input`
+* Run `./build.sh`
+* In `output`, you'll find an `ogg` music file, and a youtube-compatible single-image `mp4` file for each input.
+
+This tool just converts the files. Go to https://studio.youtube.com/ and upload the unlisted videos yourself.
+
+I use https://mp3chapters.github.io/ to create chapter markers.
---
+++ build.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#set -eux
+set -eu
+cd "`dirname "$0"`"
+
+main() {
+	TMP="./.tmp"
+	rm -rf "$TMP"
+	mkdir -p "$TMP"
+	ls ./input > ./output/input.txt
+	for IN in ./input/*; do
+		# bash sorcery. https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
+		BASE="${IN%.*}"
+		FILE="`basename "$BASE"`"
+		EXT="${IN##*.}"
+		OGG="./output/$FILE.ogg"
+		MP4="./output/$FILE.mp4"
+		#echo $IN - $BASE - $FILE - $EXT
+		if [ -f "$OGG" -a -f "$MP4" ]; then
+			echo $IN: already built, skipping
+		else
+			case "$EXT" in
+				mid)
+					echo $IN: building midi...
+					WAV="./tmp/$FILE.wav"
+					mid2wav "$IN" "$WAV"
+					wav2ogg "$WAV" "$OGG"
+					ogg2mp4 "$OGG" "$MP4"
+					rm -f "$WAV"
+					;;
+				wav)
+					echo $IN: building wav...
+					wav2ogg "$IN" "$OGG"
+					ogg2mp4 "$OGG" "$MP4"
+					;;
+				ogg)
+					echo $IN: building ogg...
+					cp -f "$IN" "$OGG"
+					ogg2mp4 "$OGG" "$MP4"
+					;;
+				mp3)
+					echo $IN: building mp3...
+					mp32ogg "$IN" "$OGG"
+					ogg2mp4 "$OGG" "$MP4"
+					;;
+				*)
+					echo $IN: oops, unrecognized file type: $EXT
+					exit 1
+					;;
+			esac
+		fi
+	done
+	rm -rf "$TMP"
+}
+
+mid2wav() {
+	# https://www.reddit.com/r/Ubuntu/comments/1dfbyyh/convert_many_midi_files_in_mp3/
+	timidity "$1" -Ow -o "$2"
+}
+
+wav2ogg() {
+	# https://superuser.com/questions/1121334/how-to-use-ffmpeg-to-encode-ogg-audio-files
+	ffmpeg -i "$1" -c:a libvorbis -b:a 64k "$2"
+}
+
+ogg2mp4() {
+	IMG="assets/youtube.png"
+	# https://stackoverflow.com/questions/25381086/convert-mp3-video-with-static-image-ffmpeg-libav-bash
+	#ffmpeg -loop 1 -i "$IMG" -i "$1" -c:a copy -c:v libx264 -shortest "$2"
+	# https://askubuntu.com/questions/1315697/could-not-find-tag-for-codec-pcm-s16le-in-stream-1-codec-not-currently-support
+	ffmpeg -loop 1 -i "$IMG" -i "$1" -c:a aac -c:v libx264 -shortest "$2"
+}
+
+mp32ogg() {
+	# mp3s are just as good as oggs, but I insist on consistency
+	# https://superuser.com/questions/273797/convert-mp3-to-ogg-vorbis
+	ffmpeg -i "$1" -c:a libvorbis -q:a 4 "$2"
+}
+
+main
